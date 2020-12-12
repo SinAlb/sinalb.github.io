57d8d775cc44364d2b16686e7221027b8df55588
SinAlb <albinasinetskaya1997@gmail.com>
2020-12-12 22:55:34 +0300

    first_deploy

---
+++ oblak.js
@@ -0,0 +1,14 @@
+VANTA.CLOUDS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  backgroundColor: 0x754f4f,
+  skyColor: 0x5e356e,
+  sunColor: 0xe3ee3,
+  sunGlareColor: 0x5632ff,
+  sunlightColor: 0x9d6838,
+  speed: 0.90
+})
